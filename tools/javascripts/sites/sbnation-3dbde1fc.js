f25ff0b238deff149408afd115d4fa2c578bc66e
Collin Rukundo <rukundocollin@gmail.com>
2017-12-06 19:02:13 +0300

    Added tools and all its children
    Includes entire site ensemble

---
+++ tools/javascripts/sites/sbnation-3dbde1fc.js
@@ -0,0 +1,85 @@
+var SBNation=SBNation || {}
+
+;
+MEME_SETTINGS= {
+    creditFont: "Helvetica",
+    creditSize: 13,
+    downloadName: "defenders-share",
+    backgroundColor: null,
+    backgroundColorOpts: ["#FFF",
+    "#F7931D",
+    "#404041",
+    "#2BABE2",
+    "#EC2529",
+    "#58585B",
+    "#487DBF",
+    "#88C541",
+    "#57B949"],
+    fontColorOpts: ["#FFF",
+    "#F7931D",
+    "#404041",
+    "#2BABE2",
+    "#EC2529",
+    "#58585B",
+    "#487DBF",
+    "#88C541",
+    "#57B949"],
+    fontStyle: "bold",
+    fontFamily: '"Druk Text", Helvetica, sans-serif',
+    fontFamilyOpts: ["Druk Text",
+    "Druk Text Wide"],
+    fontSize: 36,
+    fontSizeOpts: [ {
+        text: "Small text", value: 24
+    }
+    ,
+    {
+        text: "Medium text", value: 36
+    }
+    ,
+    {
+        text: "Large text", value: 48
+    }
+    ],
+    headUppercase: !0,
+    headlineText: "Title / Headline goes here",
+    height: 378,
+    imageScale: 1,
+    imageSrc: "",
+    overlayAlpha: .8,
+    overlayColor: "#000",
+    overlayColorOpts: ["#FFF",
+    "#F7931D",
+    "#404041",
+    "#2BABE2",
+    "#EC2529",
+    "#58585B",
+    "#487DBF",
+    "#88C541",
+    "#57B949"],
+    paddingRatio: .05,
+    textAlign: "left",
+    textAlignOpts: [ {
+        text: "Align left", value: "left"
+    }
+    ,
+    {
+        text: "Align center", value: "center"
+    }
+    ,
+    {
+        text: "Align right", value: "right"
+    }
+    ],
+    textShadow: !1,
+    textShadowEdit: !1,
+    watermarkAlpha: .8,
+    watermarkMaxWidthRatio: .25,
+    watermarkColor: "default",
+    watermarkDark: "/tools/images/site-sbnation-dark-93f4efc3.svg",
+    watermarkSrc: "/tools/images/site-sbnation-85815447.svg",
+    watermarkOpts: [],
+    width: 755
+}
+
+;
